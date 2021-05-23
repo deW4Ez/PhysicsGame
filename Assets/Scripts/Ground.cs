98f953daaa37e9a3d8286647f0fe604b75df16fa
antonoix <76112320+antonoix@users.noreply.github.com>
2021-05-23 14:56:57 +0500

    0.02

---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    public Action OnGrounded;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        OnGrounded?.Invoke();
+    }
+}
