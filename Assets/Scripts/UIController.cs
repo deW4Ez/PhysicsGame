9f3b5263149b895aee6ab71be40bbd95682435c2
antonoix <76112320+antonoix@users.noreply.github.com>
2021-06-05 18:35:58 +0500

    0.03

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -8,11 +8,16 @@ public class UIController : MonoBehaviour
 {
     public Button ButtonCut;
     public Text TextSeconds;
+    public Text TextSpeed;
+    public Button ButtonSend;
+    public Text InputHeight;
+    public Win Win;
 
     private Rope rope;
     private Ground ground;
     private bool ballIsFalling = false;
     private float startTime;
+    private bool isReady;
 
     private void Start()
     {
@@ -31,12 +36,18 @@ public class UIController : MonoBehaviour
     private void StopTimer()
     {
         ballIsFalling = false;
+        isReady = true;
     }
 
     private void Update()
     {
         if (ballIsFalling)
-            TextSeconds.text = Math.Round(Time.time - startTime, 2).ToString();
+        {
+            var time = Math.Round(Time.time - startTime, 1);
+
+            TextSeconds.text = "Время: " + Math.Round(time, 0).ToString();
+            TextSpeed.text = "Скорость: " + Math.Round((time * 9.8f), 1).ToString();
+        }
     }
 
     public void Cut()
@@ -44,5 +55,13 @@ public class UIController : MonoBehaviour
         rope.Cut();
         ballIsFalling = true;
         startTime = Time.time;
+    }
+
+    public void Send()
+    {
+        var textInput = Double.Parse(InputHeight.text);
+        var isWin = Math.Abs(textInput - 6) < 0.2f;
+        if (isReady)
+            Win.Check(isWin);
     }
 }
---
+++ Assets/Scripts/Win.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+    public Image Border;
+    public Text text;
+    public GameObject ButtonRetry;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Check(bool win)
+    {
+        if (win)
+        {
+            text.text = "ОТВЕТ ВЕРНЫЙ!";
+            Border.color = new Color32(0, 255, 47, 255);
+        }
+        else
+        {
+            text.text = "ПОПРОБУЙ ЕЩЕ РАЗ";
+            Border.color = new Color32(231, 35, 12, 255);
+            ButtonRetry.SetActive(true);
+        }
+    }
+
+    public void Retry()
+    {
+        var currScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currScene.name);
+    }
+}
